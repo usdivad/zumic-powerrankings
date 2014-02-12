9cc695f91e03eabf9ea1812d09aa0f11234d3b1c
David Su <david.d.su@gmail.com>
2014-02-12 17:32:54 -0500

    boom

--- page-rankings-test.php
+++ page-rankings-test.php
@@ -1,6 +1,6 @@
 <?php
 
-  add_filter( 'posts_orderby', 'order_by_date' );
+  add_filter( 'posts_orderby', 'order_by_multiple' );
 
   $args = array(
     'post_type' => array( 'post', 'news' ),
@@ -10,6 +10,14 @@
   query_posts( $args );
 
   remove_filter( 'posts_orderby', 'order_by_multiple' );
+?>
+
+<?php
+//Skeleton
+function get_age() {
+
+}
+
 ?>
 
 <?php $i = 1; while (have_posts()) : the_post(); ?>
@@ -29,6 +37,15 @@
 
     <div class="single-title"><?php echo $found_posts; ?><h3><?php the_title(); ?></h3></div></a>
     <div class="date"><?php echo get_the_date(); ?></div>
+    <div class="age">
+      <?php
+        $cur_time = date('U');
+        $post_time = get_post_time('U');
+        $age_hours = ($cur_time - $post_time)/(60*60);
+        //$age = (strtotime(date('U')) - strtotime(get_post_time('U')))/60;
+        //echo "Post age: ".$cur_time."-".$post_time."/60";
+        echo "Post age: ".$age_hours." hours";
+      ?>
   </div>
 
 <?php $i++; endwhile; ?>

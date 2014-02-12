be973ca97e094bd5b324270294a7586e55f3c162
David Su <david.d.su@gmail.com>
2014-02-12 16:29:55 -0500

    begin wp integration

---
+++ page-rankings-test.php
@@ -0,0 +1,33 @@
+<?php
+
+  add_filter( 'posts_orderby', 'order_by_multiple' );
+
+  $args = array(
+    'post_type' => array( 'post', 'news' ),
+    'posts_per_page' => 10
+  );
+
+  query_posts( $args );
+
+  remove_filter( 'posts_orderby', 'order_by_multiple' );
+?>
+
+<?php $i = 1; while (have_posts()) : the_post(); ?>
+  <div class="grid-3<?php if($i === 4) echo ' last'; ?> entry-content">
+
+    <a href="<?php the_permalink() ?>">
+
+    <?php
+    $args = [
+      'width' => 270,
+      'height' => 193,
+      'crop' => 1,
+      'background_fill' => 'solid'
+    ];
+    the_post_thumbnail( $args );
+    ?>
+
+    <div class="single-title"><?php echo $found_posts; ?><h3><?php the_title(); ?></h3></div></a>
+  </div>
+
+<?php $i++; endwhile; ?>

0407354f60cd59e1d5be3b5968a1247dcaac1790
David Su <david.d.su@gmail.com>
2014-02-12 13:25:03 -0500

    update algs for IFs and hn in xls

--- hot.py
+++ hot.py
@@ -3,9 +3,11 @@
 
 import math
 COOLING_RATE = 0.05
+BASE = 3
 GRAVITY = 0.5
 OFFSET = 24
 
+#Paul Graham's HN hotness algorithm
 def hn_score(score, age_hours, gravity=GRAVITY, offset_hours=OFFSET):
     if age_hours >= offset_hours:
         #return (score-1)/pow((age_hours+2), gravity) #HN verbatim
@@ -13,10 +15,15 @@ def hn_score(score, age_hours, gravity=GRAVITY, offset_hours=OFFSET):
     else:
         return score
 
-def decay(temperature, hours):
-    return temperature * math.exp(-(COOLING_RATE) * hours)
+#Newtonian cooling function, modified
+def newton_score(temperature, hours):
+    if hours >= OFFSET:
+        #return temperature * math.exp(-(COOLING_RATE) * hours)
+        return temperature * pow(BASE, (-(COOLING_RATE) * hours))
+    else:
+        return temperature
     
 base = 1000
 elapsed = 36
-print "decay: " + str(decay(base, elapsed))
+print "newton_score: " + str(newton_score(base, elapsed))
 print "hn_score: " + str(hn_score(base, elapsed))
